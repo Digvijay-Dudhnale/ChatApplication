a927b21968530fd72e57b5eb3e39c588836df0b3
Digvijay Dudhnale <dlearner421@gmail.com>
2024-09-30 20:58:24 +0530

    ProfileUpdate page UI

--- ChatApp/src/Pages/ProfileUpdate/ProfileUpdate.jsx
+++ ChatApp/src/Pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ProfileUpdate.css'
+import assets from '../../assets/assets'
 
 const ProfileUpdate = () => {
+
+    const [image,setImge] = useState(false)
+
   return (
-    <div>
-      
+    <div className='profile'>
+        <div className="profile-container">
+            <form action="">
+                <h3>Profile Details</h3>
+                <label htmlFor="avatar">
+                    <input onChange={(e)=>setImge(e.target.files[0])} type="file" id='avatar' accept='.jpg, .png, .jpeg' hidden/>
+                    <img src={image ? URL.createObjectURL(image) : assets.avatar_icon} alt="" />
+                    Upload profile image
+                </label>
+                <input type="text" placeholder='Your name' />
+                <textarea placeholder='Write profile bio' required></textarea>
+                <button type='submit'>Save</button>
+            </form>
+            <img className='profile-pic' src={image ? URL.createObjectURL(image) : assets.logo_icon} alt="" />
+        </div>
     </div>
   )
 }
