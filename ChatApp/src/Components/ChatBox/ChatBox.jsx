88ffe32b74fd774e16105459ab91c016f44cd074
Digvijay Dudhnale <dlearner421@gmail.com>
2024-10-13 22:56:23 +0530

    Adding Functionality to app 4

--- ChatApp/src/Components/ChatBox/ChatBox.jsx
+++ ChatApp/src/Components/ChatBox/ChatBox.jsx
@@ -1,50 +1,112 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './ChatBox.css'
 import assets from '../../assets/assets'
+import { AppContext } from '../../Context/AppContext'
+import { arrayUnion, doc, getDoc, onSnapshot, Timestamp, updateDoc } from 'firebase/firestore'
+import { db } from '../../Config/firebase'
+import { toast } from 'react-toastify'
 
 const ChatBox = () => {
-  return (
-    <div className='chat-box'>
-        <div className="chat-user">
-            <img src={assets.profile_img} alt="" />
-            <p>Richard Sanford <img className='dot' src={assets.green_dot} alt="" /></p>
-            <img src={assets.help_icon} className='help' alt="" />
-        </div>
 
-        <div className="chat-msg">
-            <div className="s-msg">
-                <p className="msg">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-                <div>
-                    <img src={assets.profile_img} alt="" />
-                    <p>2:40 PM</p>
-                </div>
-            </div>
-            <div className="s-msg">
-                <img src={assets.pic1} className='msg-img' alt="" />
-                <div>
-                    <img src={assets.profile_img} alt="" />
-                    <p>2:40 PM</p>
-                </div>
-            </div>
-            <div className="r-msg">
-                <p className="msg">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-                <div>
-                    <img src={assets.profile_img} alt="" />
-                    <p>2:40 PM</p>
-                </div>
-            </div>
-        </div>
+    const { userData, messagesId, chatUser, messages, setMessages } = useContext(AppContext);
+    const [input, setInput] = useState("");
 
-        <div className="chat-input">
-            <input type="text" placeholder='Send a message'/>
-            <input type="file" id='image' accept='image/png, image/jpeg' hidden/>
-            <label htmlFor="image">
-                <img src={assets.gallery_icon} alt="" />
-            </label>
-            <img src={assets.send_button} alt="" />
+    const sendMessage = async () => {
+        try {
+            if (input && messagesId) {
+                await updateDoc(doc(db, "messages", messagesId), {
+                    messages: arrayUnion({
+                        sId: userData.id,
+                        text: input,
+                        createdAt: new Date()
+                    })
+                })
+
+                const userIDs = [chatUser.rId, userData.id];
+
+                userIDs.forEach(async (id) => {
+                    const userChatsRef = doc(db, "chats", id);
+                    const userChatsSnapshot = await getDoc(userChatsRef);
+
+                    if (userChatsSnapshot.exists()) {
+                        const userChatData = userChatsSnapshot.data();
+                        const chatIndex = userChatData.chatData.findIndex((c) => c.messageId === messagesId);
+                        userChatData.chatData[chatIndex].lastMessage = input.slice(0, 30);
+                        userChatData.chatData[chatIndex].updatedAt = Date.now();
+
+                        if (userChatData.chatData[chatIndex].rId === userData.id) {
+                            userChatData.chatData[chatIndex].messageSeen = false;
+                        }
+                        await updateDoc(userChatsRef, {
+                            chatData: userChatData.chatData
+                        })
+                    }
+                })
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
+        setInput("");
+    }
+
+    const convertTimeStamp = (timestamp) =>{
+        let date = timestamp.toDate();
+        const hour = date.getHours();
+        const minute = date.getMinutes();
+        if(hour > 12){
+            return hour-12 + ":" + minute + " PM";
+        }
+        else{
+            return hour + ":" + minute + " AM";
+        }
+    }
+
+    useEffect(() => {
+        if (messagesId) {
+            const unSub = onSnapshot(doc(db, "messages", messagesId), (res) => {
+                setMessages(res.data().messages.reverse());
+            })
+            return () => {
+                unSub();
+            }
+        }
+    }, [messagesId])
+
+    return chatUser ? (
+        <div className='chat-box'>
+            <div className="chat-user">
+                <img src={chatUser.userData.avatar} alt="" />
+                <p>{chatUser.userData.name} <img className='dot' src={assets.green_dot} alt="" /></p>
+                <img src={assets.help_icon} className='help' alt="" />
+            </div>
+
+            <div className="chat-msg">
+
+                {messages.map((msg, index) => (
+                    <div key={index} className={msg.sId === userData.id ? "s-msg" : "r-msg"}>
+                        <p className="msg">{msg.text}</p>
+                        <div>
+                            <img src={msg.sId === userData.id ? userData.avatar : chatUser.userData.avatar} alt="" />
+                            <p>{convertTimeStamp(msg.createdAt)}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+            <div className="chat-input">
+                <input onChange={(e) => setInput(e.target.value)} value={input} type="text" placeholder='Send a message' />
+                <input type="file" id='image' accept='image/png, image/jpeg' hidden />
+                <label htmlFor="image">
+                    <img src={assets.gallery_icon} alt="" />
+                </label>
+                <img onClick={sendMessage} src={assets.send_button} alt="" />
+            </div>
         </div>
-    </div>
-  )
+    )
+        : <div className='chat-welcome'>
+            <img src={assets.logo_icon} alt="" />
+            <p>Chat anytime, anywherre.</p>
+        </div>
 }
 
 export default ChatBox
